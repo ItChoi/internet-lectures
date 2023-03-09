d95b7619c23a7b5e4a1e27cc1e0adf9c74ecf22e
최상현 <itchoi0429@gmail.com>
2023-03-09 22:04:50 +0900

    asd

---
+++ inflearn-spring-core/src/main/java/com/example/inflearnspringcore/InflearnSpringCoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.inflearnspringcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InflearnSpringCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InflearnSpringCoreApplication.class, args);
+	}
+
+}
---
+++ inflearn-spring-core/src/test/java/com/example/inflearnspringcore/InflearnSpringCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.inflearnspringcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InflearnSpringCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
